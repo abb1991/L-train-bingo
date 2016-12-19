97b7a331153351e371697094e51b81b8871c4aec
abb1991 <alexbills1991@gmail.com>
2016-12-18 18:05:45 -0800

    added form to bingo square component. Added function to set class of bingo square based on whether or not the state 'complete' was true/false, i.e. whether the user had clicked their square complete.

--- app/assets/javascripts/components/bingocard.es6.jsx
+++ app/assets/javascripts/components/bingocard.es6.jsx
@@ -2,7 +2,13 @@ class BingoCard extends React.Component {
   constructor() {
     super();
     this.state={
-      cards: [{subject: "I am a card", id: 1}, {subject: "I am a card, too", id: 2}, {subject: "I am a card, too", id: 3}, {subject: "I am a card, too", id: 4}, {subject: "I am a card, too", id: 5}, {subject: "I am a card, too", id: 6}, {subject: "I am a card, too", id: 7},
+      cards: [{subject: "I am a card", id: 1},
+      {subject: "I am a card, too", id: 2},
+      {subject: "I am a card, too", id: 3},
+      {subject: "I am a card, too", id: 4},
+      {subject: "I am a card, too", id: 5},
+      {subject: "I am a card, too", id: 6},
+      {subject: "I am a card, too", id: 7},
       {subject: "I am a card, too", id: 8},
       {subject: "I am a card, too", id: 9},
       {subject: "I am a card, too", id: 10},
@@ -34,31 +40,31 @@ class BingoCard extends React.Component {
       <table>
         <tbody>
           <tr>
-          {this.state.cards.slice(0,4).map(function(card,i) {
+          {this.state.cards.slice(0,5).map(function(card,i) {
             return <Card info={card} key={i} handleClick={self.props.handleClick}/>
           })
         }
           </tr>
           <tr>
-          {this.state.cards.slice(5,9).map(function(card,i) {
+          {this.state.cards.slice(5,10).map(function(card,i) {
             return <Card info={card} key={i} handleClick={self.props.handleClick}/>
           })
         }
           </tr>
           <tr>
-          {this.state.cards.slice(10,14).map(function(card,i) {
+          {this.state.cards.slice(10,15).map(function(card,i) {
             return <Card info={card} key={i} handleClick={self.props.handleClick}/>
           })
         }
           </tr>
           <tr>
-          {this.state.cards.slice(15,19).map(function(card,i) {
+          {this.state.cards.slice(15,20).map(function(card,i) {
             return <Card info={card} key={i} handleClick={self.props.handleClick}/>
           })
         }
           </tr>
           <tr>
-          {this.state.cards.slice(20,24).map(function(card,i) {
+          {this.state.cards.slice(20,25).map(function(card,i) {
             return <Card info={card} key={i} handleClick={self.props.handleClick}/>
           })
         }
