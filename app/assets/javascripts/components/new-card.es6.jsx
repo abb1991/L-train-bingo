9534c0dc217a7294291ed0e6e36edc90de261677
abb1991 <alexbills1991@gmail.com>
2016-12-20 16:44:00 -0800

    added method to new-bing-card component to update component state on each keystroke in textarea.

--- app/assets/javascripts/components/new-card.es6.jsx
+++ app/assets/javascripts/components/new-card.es6.jsx
@@ -1,16 +1,30 @@
 class NewCard extends React.Component {
   constructor(){
     super();
+    this.state={
+      card: "",
+      id: 0
+    }
+  }
 
+  handleKeyDown(e) {
+    this.setState({card: this.refs.bingoText.value,
+                  id: this.props.id});
+    var bingoCard = this.state
+
+    // $.ajax({
+    //   url: 'game',
+    //   method: 'POST',
+    //   data: {data: bingoCard}
+    // }).done((response) => {
+    //   // updated card comes back?
+    // })
   }
 
   render(){
     return (
-      <td>
-        <form >
-          <textarea placeholder="Create new bingo square..."></textarea>
-          <input className="submit-bingo-square" type='submit' value='Bingo'/>
-        </form>
+      <td onKeyDown={this.handleKeyDown.bind(this)}>
+          <textarea ref="bingoText" placeholder={this.props.id} ></textarea>
       </td>
     )
   }
