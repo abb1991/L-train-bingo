af55c6bd7f6a5391c13118034696946923e36b05
abb1991 <alexbills1991@gmail.com>
2016-12-20 20:40:10 -0800

    updated README

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new(permit_params)
+    if @user.save
+      session[:user_id] = @user.id
+      log_in = logged_in?
+      render json: {user: @user, logged_in: log_in}
+    else
+      render json: {error: "Something went wrong."}
+    end
+  end
+
+  private
+
+  def permit_params
+    binding.pry
+    params.require(:newUser).permit(:name, :password)
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+end
