64dc6cccab1e4a89c0ce8a0719deca6d226085c5
abb1991 <alexbills1991@gmail.com>
2016-12-21 10:16:12 -0800

    added model/migration for game and card

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221010112) do
+ActiveRecord::Schema.define(version: 20161221181214) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "description"
+    t.boolean  "complete"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer  "creator_id"
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["creator_id"], name: "index_games_on_creator_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
