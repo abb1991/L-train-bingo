2f8ac0acb592213359100634a7815d39ca648d04
abb1991 <alexbills1991@gmail.com>
2017-02-25 18:00:58 -0800

    added friendship table and associations to the User and Friendship models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161221185136) do
+ActiveRecord::Schema.define(version: 20170226010113) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20161221185136) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
     t.index ["game_id"], name: "index_cards_on_game_id", using: :btree
+  end
+
+  create_table "friendships", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "friend_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_friendships_on_user_id", using: :btree
   end
 
   create_table "games", force: :cascade do |t|
--- spec/javascripts/AppSpec.js
+++ spec/javascripts/AppSpec.js
@@ -9,3 +9,35 @@ describe("app", function(){
     expect(user.name).toBe("alex");
   })
 })
+
+describe("new test suite", function(){
+  var stuff;
+  var timerCallback;
+  beforeEach(function(){
+    timerCallback = jasmine.createSpy("timerCallback");
+    jasmine.clock().install();
+    stuff = "abc";
+  })
+
+  afterEach(function(){
+    jasmine.clock().uninstall();
+    stuff = "";
+  })
+
+  it("should be async", function(){
+
+    setTimeout(function(){
+      timerCallback();
+    })
+
+    expect(timerCallback).not.toHaveBeenCalled();
+
+    jasmine.clock().tick(101);
+
+    expect(timerCallback).toHaveBeenCalled();
+  })
+
+  it ("should have stuff", function(){
+    expect(stuff).toBe("abc")
+  })
+})
